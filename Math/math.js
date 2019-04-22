12cccd404aca3752a8bc0c3350dc52c809ac3901
Andi Hays <github@andihays.net>
2019-04-22 15:43:40 -0500

    added math for fibonacci

--- Math/math.js
+++ Math/math.js
@@ -24,12 +24,23 @@ function MyMath() {
     }
   }
 
+  function fibonacci(a) {
+    if (a === 0) {
+      return 0;
+    } if (a === 1) {
+      return 1;
+    } else {
+      return (fibonacci(a - 1) + fibonacci(a - 2));
+    }
+  }
+
   return {
     add,
     subtract,
     multiply,
     power,
     factorial,
+    fibonacci
   }
 
 }
