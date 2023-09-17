0c8245b54f5e7f5abc45b0cc9027e24df069f937
Chuck Hays <github@chuckhays.net>
2023-09-17 10:19:10 -0500

    Game state checking

--- Swift/TicTacToe/TicTacToeGame/TicTacToeGame/Game.swift
+++ Swift/TicTacToe/TicTacToeGame/TicTacToeGame/Game.swift
@@ -2,7 +2,18 @@ import Foundation
 
 class Game {
     
+    private var gameBoard: GameBoardImpl
+    
+    init() {
+        gameBoard = GameBoardImpl(xSize: 3, ySize: 3)
+    }
+    
     public func runGame(player1: String, player2: String, count: Int) {
+        
+    }
+    
+    private func runSingleGame(player1: Player, player2: Player) {
+        // Player1 is x, Player2 is o.
         
     }
     
--- Swift/TicTacToe/TicTacToeGame/TicTacToeGame/GameBoard.swift
+++ Swift/TicTacToe/TicTacToeGame/TicTacToeGame/GameBoard.swift
@@ -4,10 +4,15 @@ enum Value {
     case x, o, empty, invalid
 }
 
+enum GameState {
+    case playing, xWins, oWins, draw
+}
+
 protocol GameBoard {
     func valueInSpace(x: Int, y: Int) -> Value
     var xSize: Int { get }
     var ySize: Int { get }
+    var state: GameState { get }
 }
 
 class GameBoardImpl: GameBoard {
@@ -19,6 +24,74 @@ class GameBoardImpl: GameBoard {
         self.xSize = xSize
         self.ySize = ySize
         values = Array(repeating: Array(repeating: .empty, count: ySize), count:xSize)
+    }
+    
+    public var state: GameState {
+        // Check for winners.
+        // Check columns.
+        for x in 0..<xSize {
+            let firstValue = values[x][0]
+            var matches = true
+            for y in 1..<ySize {
+                if firstValue != values[x][y] {
+                    matches = false
+                    break
+                }
+            }
+            if matches {
+                return firstValue == .x ? .xWins : .oWins
+            }
+        }
+        // Check rows.
+        for y in 0..<ySize {
+            let firstValue = values[0][y]
+            var matches = true
+            for x in 1..<xSize {
+                if firstValue != values[x][y] {
+                    matches = false
+                    break
+                }
+            }
+            if matches {
+                return firstValue == .x ? .xWins : .oWins
+            }
+        }
+        // Check diagonals (only applies in square games).
+        if xSize == ySize {
+            var firstValue = values[0][0]
+            var matches = true
+            for i in 1..<xSize {
+                if firstValue != values[i][i] {
+                    matches = false
+                    break
+                }
+            }
+            if matches {
+                return firstValue == .x ? .xWins : .oWins
+            }
+            firstValue = values[xSize - 1][0]
+            matches = true
+            for i in 1..<xSize {
+                if firstValue != values[xSize - 1 - i][i] {
+                    matches = false
+                    break
+                }
+            }
+            if matches {
+                return firstValue == .x ? .xWins : .oWins
+            }
+        }
+        
+        // Check for draw.
+        for x in 0..<xSize {
+            for y in 0..<ySize {
+                guard values[x][y] != .empty else {
+                    // Found an empty spot.
+                    return .playing
+                }
+            }
+        }
+        return .draw
     }
     
     public func valueInSpace(x: Int, y: Int) -> Value {
