265831fc47293c7b1c49fa3043e7d13898b06cde
Chuck Hays <github@chuckhays.net>
2023-09-17 10:26:13 -0500

    Run a single game

--- Swift/TicTacToe/TicTacToeGame/TicTacToeGame/Game.swift
+++ Swift/TicTacToe/TicTacToeGame/TicTacToeGame/Game.swift
@@ -2,19 +2,30 @@ import Foundation
 
 class Game {
     
-    private var gameBoard: GameBoardImpl
-    
     init() {
-        gameBoard = GameBoardImpl(xSize: 3, ySize: 3)
     }
     
     public func runGame(player1: String, player2: String, count: Int) {
         
     }
     
-    private func runSingleGame(player1: Player, player2: Player) {
+    private func runSingleGame(player1: Player, player2: Player) -> GameState {
+        let gameBoard = GameBoardImpl(xSize: 3, ySize: 3)
         // Player1 is x, Player2 is o.
+        var currentTurn: Value = .x
         
+        while gameBoard.state == .playing {
+            let move = currentTurn == .x ? player1.makeMove(gameBoard: gameBoard, playerValue: .x) :
+                player2.makeMove(gameBoard: gameBoard, playerValue: .o)
+            var moveResult = gameBoard.setValue(x: move.x, y: move.y, value: currentTurn)
+            guard moveResult else {
+                // Player made an illegal move, end the game.
+                return currentTurn == .x ? .oWins : .xWins
+            }
+            currentTurn = currentTurn == .x ? .o : .x
+        }
+        
+        return gameBoard.state
     }
     
 }
